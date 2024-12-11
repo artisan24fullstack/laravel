ff16f98466750adb58d53df628954919676c717c
artisan24fullstack <160485169+artisan24fullstack@users.noreply.github.com>
2024-12-11 15:46:00 +0100

    [ADD] traductions

--- breeze/resources/views/layouts/navigation.blade.php
+++ breeze/resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('home.dashboard') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -50,7 +50,9 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                <x-traductions></x-traductions>
             </div>
+
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -68,7 +70,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('home.dashboard') }}
             </x-responsive-nav-link>
         </div>
 
